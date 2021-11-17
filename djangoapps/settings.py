e27eac81765c2b2dd41c022415736d51c80fba16
Jonathan mwenze <jonathanmwenze@gmail.com>
2021-11-17 18:33:43 +0200

    Testing the app

--- djangoapps/settings.py
+++ djangoapps/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoapps.urls'
@@ -120,6 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-django_heroku.settings(local())
+STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static')
+]
+
+#django_heroku.settings(local())
